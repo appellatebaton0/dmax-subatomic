989dee310792c5d99737a1e27037bf5969dfac69
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-05-31 15:00:35 -0500

    h1 - Export Shell & Falling Power
    Made a quick shell script to pack and export the datapack to a testing world, and added a fall modifying power.

---
+++ export.sh
@@ -0,0 +1,6 @@
+
+## Package the datapack up and ship it off to the testing world.
+
+zip dmax-subatomic-sizeling.zip data pack.mcmeta README.md -r
+
+mv dmax-subatomic-sizeling.zip '/home/Baton/.local/share/PrismLauncher/instances/origins testing/minecraft/saves/OriginsTesting/datapacks/'
